8e8ec2a4fdafd7da6a45e49bef14cf45c8aa9208
Garnett Puett and Ted Leary <garnett+ted@>
2018-06-26 16:46:00 -0700

    fin

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import './home.css'
 import Nav from './Nav.jsx'
-function Home(){
-  return(
-    <div className='home-wrapper'>
-      <Nav/>
-      <div className='home-content'>
-        <h1>Avery Organics</h1>
+
+class Home extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {isAltTitleShowing: false}
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(){
+    this.setState(prevState => ({
+      isAltTitleShowing: !prevState.isAltTitleShowing
+    }));
+  }
+  render(){
+    let altTitle = <h1>Aviary Organics</h1>;
+    if (this.state.isAltTitleShowing === true){
+      altTitle = <h1>Also Avery Organics</h1>;
+    } else {
+      altTitle = <h1>Avery Organics</h1>;
+    }
+
+    return(
+      <div className='home-wrapper'>
+        <Nav/>
+        <p onClick={this.handleClick}>click me!</p>
+        <div className='home-content'>
+          {altTitle}
+        </div>
+
+
+
       </div>
+    );
 
-
-    </div>
-  );
+  }
 }
 
 export default Home;
